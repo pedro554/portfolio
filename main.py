932f1246edf476f76bc05b14a07e2a26bc878a8e
Pedro Viero <pedrohenriqueviero@outlook.com>
2022-11-16 22:36:45 -0300

    Pagina Inicial

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, redirect, render_template, request, url_for
+
+app = Flask(__name__)
+tecnologias = 'Python, Delphi'
+facebook = 'https://www.facebook.com/profile.php?id=100002337424265'
+linkedin = 'https://www.linkedin.com/in/pedro-henrique-canterle-viero-8278771b0/'
+github = 'https://github.com/pedro554'
+nome = 'Pedro Henrique Canterle Viero'
+
+@app.route('/')
+def index():
+    return render_template('index.html', 
+                            tecnologias=tecnologias,
+                            facebook=facebook,
+                            linkedin=linkedin, 
+                            github=github,
+                            nome=nome)
+
+@app.route('/sobre')
+def sobre():    
+    return render_template('sobre.html')
+
+@app.route('/resumo')
+def resumo():
+    return render_template('resumo.html')
+
+@app.route('/portfolio')
+def portfolio(): 
+    return render_template('portfolio.html')
+
+app.run(debug=True)
